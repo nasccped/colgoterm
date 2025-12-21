ee78132fb70fcfa29cc71b556767b705d67c1e73
Nasccped <pedro.main.tec@gmail.com>
2025-12-21 10:46:27 -0300

    chore: init + call app on main

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/nasccped/colgoterm/internals/app"
+	"github.com/nasccped/colgoterm/internals/utils"
+)
 
-func intoBrightRed(s string) string {
-	return fmt.Sprintf("\x1b[92m%s\x1b[0m", s)
+var (
+	w string = "-w"
+	h string = "-h"
+	g string = "-g"
+)
+
+// Short hand FlagIdentifier unwraper for surely non-error values.
+func flagUnwrapper(long string, short *string, requireValue bool, description string) *utils.FlagIdentifier {
+	fi, err := utils.NewFlagIdentifier(long, short, requireValue, description)
+	if err != nil {
+		panic(err)
+	}
+	return fi
 }
 
 func main() {
-	fmt.Printf("Welcome to the %s!\n", intoBrightRed("colgoterm"))
+	var (
+		helpFlag   *utils.FlagIdentifier = flagUnwrapper("--help", nil, false, "Prints the help panel")
+		widthFlag  *utils.FlagIdentifier = flagUnwrapper("--width", &w, true, "Set the square width (min = 4)")
+		heightFlag *utils.FlagIdentifier = flagUnwrapper("--height", &h, true, "Set the square height (min = 4)")
+		gapFlag    *utils.FlagIdentifier = flagUnwrapper("--gap", &g, true, "Set the gap between squares (min = 4)")
+	)
+	flags := []*utils.FlagIdentifier{
+		helpFlag, widthFlag, heightFlag, gapFlag,
+	}
+	app := app.NewApp(flags)
+	app.ShowHelp()
 }
