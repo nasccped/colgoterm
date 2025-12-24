bdce90d99555fbf3141da63fcaa928deb989b09c
Nasccped <pedro.main.tec@gmail.com>
2025-12-24 15:01:05 -0300

    chore: main entrypoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/nasccped/colgoterm/internals/app"
 	"github.com/nasccped/colgoterm/internals/utils"
 )
@@ -20,16 +22,25 @@ func flagUnwrapper(long string, short *string, requireValue bool, description st
 	return fi
 }
 
+// Prints the error message + exit with error code (`1`).
+func printErrAndExit(err error) {
+	err = fmt.Errorf("\x1b[91merror\x1b[0m: %s", err)
+	fmt.Println(err)
+	os.Exit(1)
+}
+
 func main() {
 	var (
 		helpFlag   *utils.FlagIdentifier = flagUnwrapper("--help", nil, false, "Prints the help panel")
-		widthFlag  *utils.FlagIdentifier = flagUnwrapper("--width", &w, true, "Set the square width (min = 4)")
-		heightFlag *utils.FlagIdentifier = flagUnwrapper("--height", &h, true, "Set the square height (min = 4)")
-		gapFlag    *utils.FlagIdentifier = flagUnwrapper("--gap", &g, true, "Set the gap between squares (min = 4)")
+		widthFlag  *utils.FlagIdentifier = flagUnwrapper("--width", &w, true, "Set the square width (default = 8)")
+		heightFlag *utils.FlagIdentifier = flagUnwrapper("--height", &h, true, "Set the square height (default = 4)")
+		gapFlag    *utils.FlagIdentifier = flagUnwrapper("--gap", &g, true, "Set the gap between squares (default = 4)")
 	)
 	flags := []*utils.FlagIdentifier{
 		helpFlag, widthFlag, heightFlag, gapFlag,
 	}
-	app := app.NewApp(flags)
-	app.ShowHelp()
+	app := app.NewApp(flags, os.Args[1:])
+	if temp := app.Run(); temp != nil {
+		printErrAndExit(temp)
+	}
 }
